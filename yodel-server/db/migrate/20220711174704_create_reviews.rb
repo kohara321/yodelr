53ea872678acb2cd758ca506c603b94b0e5ae90f
kohara321 <101386028+kohara321@users.noreply.github.com>
2022-07-11 13:02:05 -0600

    Delete yodel-server directory

--- yodel-server/db/migrate/20220711174704_create_reviews.rb
+++
@@ -1,9 +0,0 @@
-class CreateReviews < ActiveRecord::Migration[6.1]
-  def change
-    create_table :reviews do |t|
-      t.integer :game_id,
-      t.integer :user_id,
-      t.string :review
-    end
-  end
-end
