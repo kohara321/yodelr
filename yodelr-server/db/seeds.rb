f2e68594abe10f6bca1dcb46976bcce1fee352ee
Kaegan O'Hara <kohara321@gmail.com>
2022-07-11 16:17:34 -0600

    seed

--- yodelr-server/db/seeds.rb
+++ yodelr-server/db/seeds.rb
@@ -8,7 +8,7 @@ puts "Deleting old data..."
 
 Game.destroy_all
 
-amino = Array(1..50)
+game_ids = Array(1..50)
 
 def the_method(arr)
   arr.each_with_index do |games, index|
@@ -20,45 +20,23 @@ def the_method(arr)
 end
 
 def insert(data, index)
-    # title = data["title"]
-    # puts title
     pp index
-    # pp data[“data”][“title”]
-    # fixed = index + 1
-    key = data["id"]
+    # pp data["title"]
     title = data["title"]
     thumbnail = data["thumbnail"]
-    genre = data["genre"]
+    # genre = data["genre"]
     short_description = data["short_description"]
-    description = data["description"]
+    # description = data["description"]
     publisher = data["publisher"]
     developer = data["developer"]
     release_date = data["release_date"]
     game_url = data["game_url"]
 
-    Game.create(key: key, title: title, thumbnail: thumbnail, short_description: short_description, description: description, publisher: publisher, developer: developer, release_date: release_date, game_url: game_url)
-    
-    # db = SQLite3::Database.open “table”
-    # db.execute “INSERT INTO artworks VALUES (#{index}, #{key}, ‘#{title}‘, ‘#{artist}‘, ‘#{img_link}’)”
+    Game.create(title: title, thumbnail: thumbnail, short_description: short_description, publisher: publisher, developer: developer, release_date: release_date, game_url: game_url)
+    sleep 4
 end
 
-the_method(amino)
-
-# def games
-#     response = RestClient.get("https://www.freetogame.com/api/games")
-#     json = JSON.parse response
-
-#     if !json.nil?
-#         json["data"].map do |game| 
-#             Game.create(title: "#{game["title"]}")
-#         end
-#     else
-#         puts "Error seeding"
-#     end
-# end
-
-
-
+the_method(game_ids)
 
 
 puts "Done seeding..."
