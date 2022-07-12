5c14f05f9a36935317870d86ce720efc33adf1f7
Luis Ibarra <luis9609@gmail.com>
2022-07-12 13:02:02 -0600

    added get requests

--- yodelr-server/app/controllers/application_controller.rb
+++ yodelr-server/app/controllers/application_controller.rb
@@ -11,4 +11,28 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  get "/games" do
+    games = Game.all 
+    game.to_json
+  end
+
+  get "/publisher" do
+    game = Game.find_all(:publisher)
+    game.to_json
+  end
+
+  get "/developer" do
+    game = Game.find_all(:developer)
+    game.to_json
+  end
+
+  get "/release" do
+    game = Game.find_all(:release_date)
+    game.to_json
+  end
+
+
+
+
+
 end
