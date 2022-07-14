46005feddbf1d240a3a9b0dc9c7cf776ce2b0246
Luis Ibarra <luis9609@gmail.com>
2022-07-14 12:47:59 -0600

    added submit data form to the SQL database reviewed_games

--- yodelr-server/db/migrate/20220711185053_create_reviews.rb
+++ yodelr-server/db/migrate/20220711185053_create_reviews.rb
@@ -2,7 +2,6 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
       t.integer :game_id
-      t.integer :user_id
       t.string :review
     end
   end
